a01b15a0405191ecb44e033447a8693269bb63d1
Erlon Jr. <erlon.dn.jr@gmail.com>
2026-04-09 17:42:23 -0300

    CORS

--- src/main/java/Servidor.java
+++ src/main/java/Servidor.java
@@ -11,6 +11,25 @@ import static spark.Spark.*;
 
 public class Servidor {
 
+    private static void enableCORS() {
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        });
+    }
+
     private static BufferedImage binarizarImagem(BufferedImage imagem){
         BufferedImage imagemCinza = Processador.paraPB(imagem);
         int limiar = 127;
@@ -20,6 +39,7 @@ public class Servidor {
     public static void main() {
 
         port(8080);
+        enableCORS();
 
         post("/binarizacao", (Request request, Response response) -> {
 
